7a02f24a12c27c97da0f72b651b685ca4612f729
Eugen-Alexander Habrony <info@habrony.de>
2025-07-14 15:59:47 +0200

    Setting up stripe on the client and creating the RTK query api for checkout

--- client/src/app/api/baseApi.ts
+++ client/src/app/api/baseApi.ts
@@ -4,8 +4,9 @@ import { startLoading, stopLoading } from "../layout/uiSlice";
 import { toast } from "react-toastify";
 import { router } from "../routes/Routes";
 
+
 const customBaseQuery = fetchBaseQuery({
-    baseUrl: 'https://localhost:5001/api',
+    baseUrl: import.meta.env.VITE_API_URL,
     credentials: 'include'
 });
 
--- client/src/features/checkout/CheckoutPage.tsx
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -1,12 +1,42 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import OrderSummary from "../../app/shared/components/OrderSummary";
 import CheckoutStepper from "./CheckoutStepper";
+import { loadStripe, type StripeElementsOptions } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useFetchBasketQuery } from "../basket/basketApi";
+import { useEffect, useMemo, useRef } from "react";
+import { useCreatePaymentIntentMutation } from "./checkoutApi";
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
 
 export default function CheckoutPage() {
+  const {data: basket} = useFetchBasketQuery();
+  const [createPaymentIntent, {isLoading}] = useCreatePaymentIntentMutation();
+  const created = useRef(false);
+
+  useEffect(() => {
+    if(!created.current) createPaymentIntent();
+    created.current = true;
+  },[createPaymentIntent]);
+
+  const options: StripeElementsOptions | undefined = useMemo(() => {
+    if(!basket?.clientSecret) return undefined;
+    return {
+      clientSecret: basket.clientSecret
+    }
+  },[basket?.clientSecret]);
+
+
   return (
     <Grid container spacing={2}>
       <Grid size={8}>
-        <CheckoutStepper />
+        {!stripePromise || !options || isLoading ? (
+          <Typography variant="h6">Loading checkout...</Typography>
+        ) : (
+        <Elements stripe={stripePromise} options={options}>
+          <CheckoutStepper />
+        </Elements>
+        )}
       </Grid>
       <Grid size={4}>
         <OrderSummary />
