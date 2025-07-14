e1c161e7a49a51502597027c9090a39fef7f6e1c
Eugen-Alexander Habrony <info@habrony.de>
2025-07-14 16:12:41 +0200

    Adding the payment element

--- client/src/features/checkout/CheckoutStepper.tsx
+++ client/src/features/checkout/CheckoutStepper.tsx
@@ -1,4 +1,6 @@
-import { Box, Button, Paper, Step, StepLabel, Stepper } from "@mui/material";
+import { CheckBox } from "@mui/icons-material";
+import { Box, Button, FormControlLabel, Paper, Step, StepLabel, Stepper } from "@mui/material";
+import { AddressElement } from "@stripe/react-stripe-js";
 import { useState } from "react"
 
 const steps = ['Address', 'Payment', 'Review'];
@@ -29,7 +31,17 @@ export default function CheckoutStepper() {
 
         <Box sx={{mt: 2}}>
             <Box sx={{display: activeStep === 0 ? 'block' : 'none'}}>
-                Address step
+                <AddressElement
+                    options={{
+                        mode: 'shipping',
+
+                    }}
+                />
+                <FormControlLabel
+                    sx={{display: 'flex', justifyContent: 'end'}}
+                    control={<CheckBox />}
+                    label='Save as default'
+                />
             </Box>
             <Box sx={{display: activeStep === 1 ? 'block' : 'none'}}>
                 Payment step
