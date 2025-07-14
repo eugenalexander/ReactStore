cf4da1f7e3933b8277d3b2cb704260db0dad21ce
Eugen-Alexander Habrony <info@habrony.de>
2025-07-14 15:21:01 +0200

    Creating a stepper component

--- client/src/features/checkout/CheckoutPage.tsx
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -1,11 +1,12 @@
 import { Grid } from "@mui/material";
 import OrderSummary from "../../app/shared/components/OrderSummary";
+import CheckoutStepper from "./CheckoutStepper";
 
 export default function CheckoutPage() {
   return (
     <Grid container spacing={2}>
       <Grid size={8}>
-        Checkout stepper goes here
+        <CheckoutStepper />
       </Grid>
       <Grid size={4}>
         <OrderSummary />
