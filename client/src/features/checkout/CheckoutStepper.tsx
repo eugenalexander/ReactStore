0d54b44803a74ae7758b1ceafe9630f4aa95deb5
Eugen-Alexander Habrony <info@habrony.de>
2025-07-16 19:28:33 +0200

    Getting confirmation token from stripe & confirming the payment

--- client/src/features/checkout/CheckoutStepper.tsx
+++ client/src/features/checkout/CheckoutStepper.tsx
@@ -1,17 +1,21 @@
 import { CheckBox } from "@mui/icons-material";
 import { Box, Button, FormControlLabel, Paper, Step, StepLabel, Stepper, Typography } from "@mui/material";
-import { AddressElement, PaymentElement, useElements } from "@stripe/react-stripe-js";
+import { LoadingButton } from "@mui/lab";
+import { AddressElement, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useState } from "react"
 import Review from "./Review";
 import { useFetchAddressQuery, useUpdateUserAddressMutation } from "../accounts/accountApi";
-import type { StripeAddressElementChangeEvent, StripePaymentElementChangeEvent } from "@stripe/stripe-js";
+import { type ConfirmationToken, type StripeAddressElementChangeEvent, type StripePaymentElementChangeEvent } from "@stripe/stripe-js";
 import { useBasket } from "../../lib/hooks/useBasket";
 import { currencyFormat } from "../../lib/util";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const steps = ['Address', 'Payment', 'Review'];
 
 export default function CheckoutStepper() {
     const[activeStep, setActiveStep] = useState(0);
+    const {basket} = useBasket();
     const {data, isLoading} = useFetchAddressQuery();    
  
     let name, restAddress;
@@ -23,9 +27,13 @@ export default function CheckoutStepper() {
     const [updateAddress] = useUpdateUserAddressMutation()
     const [saveAddressChecked, setSaveAddressChecked] = useState(false);
     const elements = useElements();
+    const stripe = useStripe()
     const [addressComplete, setAddressComplete] = useState(false);
     const [paymentComplete, setPaymentComplete] = useState(false);
-    const{total} = useBasket();
+    const [submiting, setSubmiting] = useState(false);
+    const{total, clearBasket} = useBasket();
+    const navigate = useNavigate();
+    const [confirmationToken, setConfirmationToken] = useState<ConfirmationToken | null>(null);
 
 
     const handleNext = async () => {
@@ -33,11 +41,54 @@ export default function CheckoutStepper() {
             const address = await getStripeAddress()
             if(address) await updateAddress(address);
         } 
+        if(activeStep === 1) {
+            if(!elements || !stripe) return;
+            const result = await elements.submit();
+            if(result.error) return toast.error(result.error.message);
+
+            const stripeResult = await stripe.createConfirmationToken({elements});
+            if(stripeResult.error) return toast.error(stripeResult.error.message);
+            setConfirmationToken(stripeResult.confirmationToken); 
+        }
+        if(activeStep === 2) {
+            await confirmPayment();
+        }
         setActiveStep(step => step + 1);
     }
 
     const handleBack = () => {
         setActiveStep(step => step - 1);
+    }
+
+    const confirmPayment = async () => {
+        setSubmiting(true);
+        try {
+            if(!confirmationToken || !basket?.clientSecret) throw new Error('Unable to process payment');
+
+            const paymentResult = await stripe?.confirmPayment({
+                clientSecret: basket.clientSecret,
+                redirect: 'if_required',
+                confirmParams: {
+                    confirmation_token: confirmationToken.id
+                }
+            });
+            if(paymentResult?.paymentIntent?.status === 'succeeded') {
+                navigate('/checkout/success');
+                clearBasket();
+            } else if(paymentResult?.error) {
+                throw new Error(paymentResult.error.message);
+            } else {
+                throw new Error('Something went wrong');
+            }
+        } catch (error) {
+            if(error instanceof Error) {
+                toast.error(error.message)
+            }
+            setActiveStep(step => step - 1);
+            
+        } finally {
+            setSubmiting(false);
+        }
     }
 
     const getStripeAddress = async () => {
@@ -98,20 +149,21 @@ export default function CheckoutStepper() {
                 <PaymentElement onChange={handlePaymentChange} />
             </Box>
             <Box sx={{display: activeStep === 2 ? 'block' : 'none'}}>
-                <Review />
+                <Review confirmationToken={confirmationToken} />
             </Box>
         </Box>
 
         <Box display='flex' paddingTop={2} justifyContent='space-between'>
-            <Button 
+            <LoadingButton 
                 onClick={handleBack}
                 disabled={
                     (activeStep === 0 && !addressComplete) ||
-                    (activeStep === 1 && ! paymentComplete)
+                    (activeStep === 1 && ! paymentComplete) ||
+                    submiting
                 }
             >
                 {activeStep === steps.length - 1 ? `Pay ${currencyFormat(total)}` : 'Next'}
-            </Button>
+            </LoadingButton>
             <Button onClick={handleNext}>Next</Button>
         </Box>
     </Paper>
--- client/src/features/checkout/Review.tsx
+++ client/src/features/checkout/Review.tsx
@@ -1,9 +1,28 @@
 import { Box, Divider, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
-import { useFetchBasketQuery } from "../basket/basketApi"
 import { currencyFormat } from "../../lib/util";
+import type { ConfirmationToken } from "@stripe/stripe-js";
+import { useBasket } from "../../lib/hooks/useBasket";
 
-export default function Review() {
-    const{data: basket} = useFetchBasketQuery();
+type Props = {
+    confirmationToken: ConfirmationToken | null;
+}
+
+export default function Review({confirmationToken}: Props) {
+    const{basket} = useBasket();
+
+    const addressString = () => {
+        if(!confirmationToken?.shipping) return ''; 
+        const {name, address} = confirmationToken.shipping;
+        return `${name}, ${address?.line1}, ${address?.city}, ${address?.state},
+         ${address?.postal_code}, ${address?.country}`
+    }
+    const paymentString = () => {
+        if(!confirmationToken?.payment_method_preview.card) return '';
+        const {card} = confirmationToken.payment_method_preview;
+        
+        return `${card.brand.toUpperCase}, **** **** **** ${card.last4},
+            Exp: ${card.exp_month}/${card.exp_year}`;
+    }
 
   return (
     <div>
@@ -16,13 +35,13 @@ export default function Review() {
                     Shipping address
                 </Typography>
                 <Typography component='dd' mt={1} color='textSecondary'>
-                    Address goes here
+                    {addressString()}
                 </Typography>
                 <Typography component='dt' fontWeight='medium'>
                     Payment details
                 </Typography>
                 <Typography component='dd' mt={1} color='textSecondary'>
-                    Payment details goes here
+                    {paymentString()}
                 </Typography>
             </dl>
         </Box>
